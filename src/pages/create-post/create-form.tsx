c919b3c241fea525e580e3a02dd8903e6b365be4
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-08-10 22:12:16 +0530

    update

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { addDoc, collection } from 'firebase/firestore'
-import { auth, db } from '../../config/firebase'
-import { useAuthState } from 'react-firebase-hooks/auth'
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 interface CreateFormData {
   title: string;
@@ -12,52 +12,41 @@ interface CreateFormData {
 }
 
 export const CreateForm = () => {
-
-  const [user] = useAuthState(auth)
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   const schema = yup.object().shape({
     title: yup.string().required("You must add a title."),
     description: yup.string().required("You must add a description."),
-  })
-
-  const postsRef = collection(db, "posts")
+  });
 
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm<CreateFormData>({
-    resolver: yupResolver(schema)
-  })
+    resolver: yupResolver(schema),
+  });
+
+  const postsRef = collection(db, "posts");
 
   const onCreatePost = async (data: CreateFormData) => {
-    // console.log(data)
     await addDoc(postsRef, {
-      // title: data.title,
-      // description: data.description,
       ...data,
       username: user?.displayName,
-      userId: "user?.uid",
-    })
-  }
+      userId: user?.uid,
+    });
 
-
-
-
-
-
+    navigate("/");
+  };
 
   return (
-
-    <form action="" onSubmit={handleSubmit(onCreatePost)}>
-      <input type="text" placeholder='Title...' {...register("title")} />
-      <p style={{ color: "red" }}>{errors.title?.message}</p>
-
-      <textarea placeholder='Description...' {...register("description")} />
-      <p style={{ color: "red" }}>{errors.description?.message}</p>
-
-      <input type='submit' className='formButton' />
+    <form onSubmit={handleSubmit(onCreatePost)}>
+      <input placeholder="Title..." {...register("title")} />
+      <p style={{ color: "red" }}> {errors.title?.message}</p>
+      <textarea placeholder="Description..." {...register("description")} />
+      <p style={{ color: "red" }}> {errors.description?.message}</p>
+      <input type="submit" className="submitForm" />
     </form>
-
-  )
-}
+  );
+};
