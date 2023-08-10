cb64aeb9f8d61127998c39a559f758068118f682
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-08-10 20:53:20 +0530

    update

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+interface CreateFormData {
+  title: string;
+  description: string;
+}
 
 export const CreateForm = () => {
+  const schema = yup.object().shape({
+    title: yup.string().required("You must add a title."),
+    description: yup.string().required("You must add a description."),
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<CreateFormData>({
+    resolver: yupResolver(schema)
+  })
+
+  const onCreatePost = (data: CreateFormData) => {
+    console.log(data)
+  }
+
+
+
+
+
+
+
   return (
     <div>
-        <form action="">
-          
-        </form>
+      <form action="" onSubmit={handleSubmit(onCreatePost)}>
+        <input type="text" placeholder='Title...' {...register("title")} />
+        <p style={{ color: "red" }}>{errors.title?.message}</p>
+
+        <textarea placeholder='Description...' {...register("description")} />
+        <p style={{ color: "red" }}>{errors.description?.message}</p>
+
+        <input type='submit' />
+      </form>
     </div>
   )
 }
