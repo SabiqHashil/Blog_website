f18ff873bf68a0cc018609f74dc0da875e116dbd
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-08-10 13:23:03 +0530

    update

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCtE3ZVQeF2Ht-EB_vi8kNhcsC2A5k3tc",
+  authDomain: "social-media-web-fd460.firebaseapp.com",
+  projectId: "social-media-web-fd460",
+  storageBucket: "social-media-web-fd460.appspot.com",
+  messagingSenderId: "810536249155",
+  appId: "1:810536249155:web:05b5f23fa09444cfe8b1f6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
