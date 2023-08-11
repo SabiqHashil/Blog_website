84f2c399080a17e542f65cacce3d14b453c90de6
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-08-11 20:42:35 +0530

    update

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Post as IPost } from './main'
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, getDocs, collection, query, where } from 'firebase/firestore';
 import { auth, db } from '../../config/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
@@ -8,15 +8,34 @@ interface Props {
     post: IPost;
 }
 
+interface Like {
+    userId: string;
+}
+
 export const Post = (props: Props) => {
     const { post } = props;
     const [user] = useAuthState(auth)
 
+    const [likes, setLikes] = useState<Like[] | null>(null)
+
     const likesRef = collection(db, "likes")
 
-    const addLike = async () => {
-        await addDoc(likesRef, { userId: user?.uid, postId: })
+    const likesDoc = query(likesRef, where("postId", "==", post.id))
+
+    const getLikes = async () => {
+        const data = await getDocs(likesDoc)
+        setLikes(data.docs.map((doc) => ({ userId: doc.data().userId })))
     }
+    const addLike = async () => {
+        await addDoc(likesRef, { userId: user?.uid, postId: post.id });
+        setLikes((prev)=> [...prev, {userId: }])
+    }
+
+    const hasUserLiked = likes?.find((like) => like.userId === user?.uid)
+
+    useEffect(() => {
+        getLikes()
+    }, [])
 
     return (
         <div>
@@ -29,8 +48,11 @@ export const Post = (props: Props) => {
             <div className='footer'>
                 <p>{post.username}</p>
                 <button
-
-                > &#128077; </button>
+                    onClick={addLike}
+                >
+                    {hasUserLiked ? <> &#128078; </> : <> &#128077; </>}
+                </button>
+                {likes && <p>Likes: {likes?.length}</p>}
             </div>
         </div>
     )
